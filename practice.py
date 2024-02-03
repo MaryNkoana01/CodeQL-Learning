1f7e861571619966a07e7ff739abe938e5bb7c2e
MaryNkoana01 <143197641+MaryNkoana01@users.noreply.github.com>
2024-02-03 21:57:41 +0200

    Update practice.py
    added error  handling

--- practice.py
+++ practice.py
@@ -1,5 +1,13 @@
 def divide_numbers(a, b):
-    return a / b
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+        return None
 
 result = divide_numbers(10, 0)
-print(f"The result is: {result}")
+
+if result is not None:
+    print(f"The result is: {result}")
+
