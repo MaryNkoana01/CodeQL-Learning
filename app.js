06b3c2da67d9b11adb5c56c02a073d927a9f1ec4
MaryNkoana01 <143197641+MaryNkoana01@users.noreply.github.com>
2024-02-14 10:23:22 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function login(username, password) {
-  if (username === "admin" && password === "password123") {
-    localStorage.setItem("isLoggedIn", "true");
-    alert("Login successful!");
-    window.location.href = "dashboard.html";
-  } else {
-    alert("Invalid username or password.");
-  }
-}
